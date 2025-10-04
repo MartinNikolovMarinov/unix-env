0f611b7479c32476f2f5cb88d410e9af44ad1a97
MartinNikolovMarinov <martin_n_marinov@protonmail.com>
2025-10-04 17:39:45 +0300

    gnome configurations

--- scripts/arch-wayland/install_gnome.sh
+++ scripts/arch-wayland/install_gnome.sh
@@ -5,3 +5,9 @@ sudo pacman -S gnome gnome-shell gnome-session gdm
 # Enable services
 sudo systemctl enable --now NetworkManager
 sudo systemctl enable gdm
+
+# Modify the settings for Alt+Tab behaviour
+gsettings set org.gnome.desktop.wm.keybindings switch-applications "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Shift><Alt>Tab']"
