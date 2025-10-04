42d125131d0d4b99ef227c2a6858050a70f5af85
MartinNikolovMarinov <martin_n_marinov@protonmail.com>
2025-10-04 18:09:27 +0300

    redacting some information

--- scripts/arch-wayland/install_gnome.sh
+++ scripts/arch-wayland/install_gnome.sh
@@ -6,8 +6,11 @@ sudo pacman -S gnome gnome-shell gnome-session gdm
 sudo systemctl enable --now NetworkManager
 sudo systemctl enable gdm
 
-# Modify the settings for Alt+Tab behaviour
+# Modify the settings for Alt+Tab behavior:
 gsettings set org.gnome.desktop.wm.keybindings switch-applications "[]"
 gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "[]"
 gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
 gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Shift><Alt>Tab']"
+
+# Add a minimize and maximize buttons to windows:
+gsettings set org.gnome.desktop.wm.preferences button-layout :minimize,maximize,close
